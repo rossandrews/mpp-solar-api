ef19c5d8b2c842f077dc8470adc5b16a0d278830
Ross Andrews <rossmandrews@gmail.com>
2022-01-09 10:22:51 -0800

    First commit, need to do some testing on the pi connected to the mpp-solar hardware

---
+++ README.md
@@ -0,0 +1,15 @@
+This is a small example service intended to work along side https://github.com/jblance/mpp-solar. You can send mpp-solar commands and get a response returned in a JSON format.
+
+### Setup
+* Setup mpp-solar to run in daemon mode with the config found in this project [mpp-solar.config](mpp-solar.config)
+* Have a MQTT broker running locally
+* This service must also be running on the same host as the mpp-solar but that can be changed by modifying the MQTT broker settings
+
+### Node steps
+## Install dependencies
+
+npm install
+
+## Run service
+
+node ./index.js
---
+++ index.js
@@ -0,0 +1,63 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "*", //TODO: lock this down with a proper whitelist
+  })
+);
+
+var mqtt = require("mqtt");
+
+const port = 3002
+app.listen(port, function () {
+  console.log("Example app listening at port " + port);
+});
+
+app.post("/sendCommand", (req, res, next) => {
+  var command = req.body.command;
+  console.log("Command received: " + command);
+
+  var commandTopic = "mpp-solar/api-client";
+  
+  //This can be changed if you want to use a different topic for the response
+  var responseTopicBase = commandTopic+"/requests/"; 
+  var responseTopic = responseTopicBase + command;
+
+  console.log("ResponseTopic: " + responseTopic);
+  var clientCommand = mqtt.connect("mqtt://localhost", {
+    clientId: "demo-client",
+  });
+  clientCommand.on("connect", function () {
+    clientCommand.subscribe([responseTopic], { qos: 0 });
+  });
+  
+  clientCommand.on("message", function (topic, message, packet) {
+    console.log(message.toString())
+    if (topic == responseTopic) {
+      res.json(message.toString());
+    }
+
+    clientCommand.end();
+  });
+
+  const commandObj = {command: command, topic: responseTopicBase};
+  const commandJSON = JSON.stringify(commandObj);
+  console.log(JSON.stringify(commandObj));
+
+  clientCommand.publish(commandTopic, commandJSON);
+
+  timer = setTimeout(function () {
+    if (!messageSent) {
+      console.log("timeout");
+      res.status(200).json("no response");
+      res.end();
+    }
+  }, 10000);
+});
+
+
